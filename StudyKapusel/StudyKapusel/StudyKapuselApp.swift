7ec30ecb1ad6cff3ab9c62590a365a0afea982c2
yuzurihara39 <103715108+yuzurihara39@users.noreply.github.com>
2022-11-17 16:03:53 +0900

    Add files via upload

---
+++ StudyKapusel/StudyKapusel/StudyKapuselApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyKapuselApp.swift
+//  StudyKapusel
+//
+//  Created by cmStudent on 2022/11/17.
+//
+
+import SwiftUI
+
+@main
+struct StudyKapuselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
